b09d5784aab4e4f6dd3d804fd90f938d90bd8c40
eunnbyeol <113367529+eunnbyeol@users.noreply.github.com>
2022-11-08 16:53:06 +0900

    04

--- main.c
+++ main.c
@@ -4,12 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
+	char str[30] = "happy C programming";
 	
-	strcpy(dst, src);
+	printf("문자열%s 의 길이: %i\n", str, strlen(str));
 	
-	printf("복사본 : %s", dst);
 	
 	return 0;
 }
